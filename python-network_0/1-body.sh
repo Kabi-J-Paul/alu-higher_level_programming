b9f52daf3982e87353358c3e143f798c352ff289
Kabi-J-Paul <k.kabi@alustudent.com>
2026-07-31 17:11:53 -0700

    Follow redirects in 1-body.sh

--- python-network_0/1-body.sh
+++ python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Sends a GET request to the URL and displays the body only for a 200 response
-curl -sf "$1"
+# Sends a GET request to the URL, follows redirects and displays the body of a 200 response
+curl -sfL "$1"
