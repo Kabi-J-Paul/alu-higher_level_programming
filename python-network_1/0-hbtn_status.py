8c4c87e0de1f9172b62f2e4a56e1a8f1073ef254
Kabi-J-Paul <k.kabi@alustudent.com>
2026-07-31 17:28:59 -0700

    Use intranet.hbtn.io URL in status scripts

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-"""Fetches the ALU intranet status page using the urllib package."""
+"""Fetches the intranet status page using the urllib package."""
 import urllib.request
 
 
 if __name__ == "__main__":
-    url = 'https://alu-intranet.hbtn.io/status'
+    url = 'https://intranet.hbtn.io/status'
     with urllib.request.urlopen(url) as response:
         body = response.read()
         print("Body response:")
