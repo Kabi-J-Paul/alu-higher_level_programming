88340e3918670e22bb36336938f423b727576b4a
Kabi-J-Paul <k.kabi@alustudent.com>
2026-07-31 17:15:43 -0700

    Fix header in 4-header.sh

--- python-network_0/4-header.sh
+++ python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Sends a GET request with a custom user id header and displays the body
+# Sends a GET request with the user id header and displays the body of the response
 curl -s -H "X-HolbertonSchool-User-Id: 98" "$1"
